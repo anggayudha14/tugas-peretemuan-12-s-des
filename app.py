1810ecff8b617fcfc1612e109cd278c336778d96
anggayudha14 <anggaisgantara27@gmail.com>
2026-06-16 00:27:45 +0700

    Fix os import

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from sdes import encrypt, decrypt
+import os
 
 app = Flask(__name__)
 
@@ -40,9 +41,13 @@ def index():
         mode=mode
     )
 
-if __name__ == "__main__":
+
     import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=True
+    )
